6103f381f3ae28ddabc1fff0b5ca840b61444488
Steve MacKinnon <smackinnon@izotope.com>
2022-09-28 21:47:47 -0400

    Add a basic sequencer that plays a bleep every 4 steps

--- sequencer-client/src/audio/AudioEngine.ts
+++ sequencer-client/src/audio/AudioEngine.ts
@@ -1,21 +1,57 @@
+const scheduleAheadTimeSecs: number = 0.1;
+const lookaheadMs = 25.0;
+
 export default class AudioEngine {
   private readonly _context: AudioContext;
-  private readonly _masterInNode: GainNode;
-  private readonly _oscillatorNode: OscillatorNode;
 
-  private _playing = false;
+  private _playing: boolean = false;
+  private _currentNote: number = 0;
+  private _nextNoteTime: number = 0.0; // when the next note is due.
+  private readonly _tempo: number = 127.0;
+  private readonly _numSteps: number = 16;
+  private readonly _stepsEnabled: boolean[] = new Array<boolean>(
+    this._numSteps
+  );
+
+  private _timerID: any = undefined;
+
+  private _scheduleNoteForStep(stepNumber: number, time: number): void {
+    if (!this._stepsEnabled[stepNumber]) {
+      return;
+    }
+    this._playNote(time, 420);
+  }
+
+  private _advanceStep(): void {
+    const secondsPerStep = 60.0 / this._tempo / 4.0;
+
+    this._nextNoteTime += secondsPerStep; // Add step length to last step time
+
+    // Advance the beat number, wrap to zero when reaching 4
+    this._currentNote = (this._currentNote + 1) % this._numSteps;
+  }
+
+  private _runNoteScheduler(): void {
+    // While there are notes that will need to play before the next interval,
+    // schedule them.
+    while (
+      this._nextNoteTime <
+      this._context.currentTime + scheduleAheadTimeSecs
+    ) {
+      this._scheduleNoteForStep(this._currentNote, this._nextNoteTime);
+      this._advanceStep();
+    }
+    this._timerID = setTimeout(() => this._runNoteScheduler(), lookaheadMs);
+  }
 
   constructor() {
     this._context = new AudioContext();
-    this._masterInNode = this._context.createGain();
-    this._oscillatorNode = new OscillatorNode(this._context, {
-      frequency: 320,
-      type: "sawtooth",
-    });
-    this._oscillatorNode.start();
 
-    this._oscillatorNode.connect(this._masterInNode);
-    this._masterInNode.connect(this._context.destination);
+    this._stepsEnabled.fill(false);
+    this._stepsEnabled[0] = true;
+    this._stepsEnabled[4] = true;
+    this._stepsEnabled[8] = true;
+    this._stepsEnabled[12] = true;
   }
 
   get playing(): boolean {
@@ -31,11 +67,15 @@ export default class AudioEngine {
         .resume()
         .then(() => {
           this._playing = true;
+          this._currentNote = 0;
+          this._nextNoteTime = this._context.currentTime;
+          this._runNoteScheduler();
         })
         .catch((e: any) => {
           console.log(e);
         });
     } else {
+      clearTimeout(this._timerID);
       this._context
         .suspend()
         .then(() => {
@@ -45,5 +85,15 @@ export default class AudioEngine {
           console.log(e);
         });
     }
+  }
+
+  private _playNote(startTime: number, frequency: number): void {
+    const osc = new OscillatorNode(this._context, {
+      type: "sine",
+      frequency,
+    });
+    osc.connect(this._context.destination);
+    osc.start(startTime);
+    osc.stop(startTime + 0.1);
   }
 }
