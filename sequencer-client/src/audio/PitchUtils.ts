bb3cf2c19a67a8f3ac09d2d038cadc41072427bf
Steve MacKinnon <smackinnon@izotope.com>
2022-09-30 09:21:52 -0400

    Fix semitoneToHz function

--- sequencer-client/src/audio/PitchUtils.ts
+++ sequencer-client/src/audio/PitchUtils.ts
@@ -1,5 +1,16 @@
-const REFERENCE_C = 440.0 * Math.pow(2.0, -(9 + 12 * 4) / 12.0);
-
+/**
+ *
+ * @param semitone is an offset from C3
+ * @returns the value in Hz
+ *
+ */
 export function semitoneToHz(semitone: number): number {
-  return REFERENCE_C * Math.pow(2.0, semitone / 12.0);
+  // Source: https://pages.mtu.edu/~suits/NoteFreqCalcs.html
+  // fn = f0 * (a)^n
+  // where
+  // f0 = the frequency of one fixed note which must be defined - we use A4 at 440 Hz.
+  // n = the number of half steps away from the fixed note you are.
+  //     If you are at a higher note, n is positive. If you are on a lower note, n is negative.
+  // fn = the frequency of the note n half steps away.
+  return 440.0 * Math.pow(1.059463094359, semitone);
 }
