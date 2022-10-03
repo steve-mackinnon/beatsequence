448db85c3392ad8c77b4a6e54fbbd95127d0e1e1
Steve MacKinnon <smackinnon@izotope.com>
2022-10-03 09:59:11 -0400

    Use an enum to describe each track type and get rid of explicit numTracks setters

--- sequencer-client/src/audio/SequencerEngine.ts
+++ sequencer-client/src/audio/SequencerEngine.ts
@@ -8,12 +8,20 @@ function randomPitch(): number {
   return Math.floor(Math.random() * (36 * 2) - 36);
 }
 
+enum TrackType {
+  Kick,
+  Snare,
+  ClosedHH,
+  SineBleep,
+  SquareBleep,
+}
+
 class TrackState {
   steps: StepState[];
-  oscType: OscillatorType = "sine";
-  isKick: boolean = false;
+  generatorType: TrackType;
 
-  constructor(numSteps: number) {
+  constructor(numSteps: number, generatorType: TrackType) {
+    this.generatorType = generatorType;
     this.steps = new Array<StepState>(numSteps);
     for (const stepIndex of Array(numSteps).keys()) {
       const active = Math.random() > 0.5;
@@ -35,16 +43,17 @@ export default class SequencerEngine {
   private _nextNoteTime: number = 0.0; // when the next note is due.
   private readonly _tempo: number = 127.0;
   private readonly _numSteps: number = 16;
-  private readonly _numTracks: number = 3;
-
   private readonly _tracks: TrackState[];
+  readonly numTracks: number;
+
   constructor() {
-    this._tracks = new Array<TrackState>(this._numTracks);
-    for (const trackIndex of Array(this._numTracks).keys()) {
-      this._tracks[trackIndex] = new TrackState(this._numSteps);
+    // Object.keys() returns 2 keys per enumeration for numeric enums,
+    // which is why the / 2 is necessary here.
+    this.numTracks = Object.keys(TrackType).length / 2;
+    this._tracks = new Array<TrackState>(this.numTracks);
+    for (const trackIndex of Array(this.numTracks).keys()) {
+      this._tracks[trackIndex] = new TrackState(this._numSteps, trackIndex);
     }
-    this._tracks[2].oscType = "square";
-    this._tracks[0].isKick = true;
   }
 
   setAudioEngine(audioEngine: AudioEngine): void {
@@ -77,24 +86,6 @@ export default class SequencerEngine {
     return this._currentStep;
   }
 
-  setNumTracks(numTracks: number): void {
-    if (numTracks < 2) {
-      throw Error("setNumTracks() does not accept numTracks less than 2.");
-    }
-    if (numTracks < this._tracks.length) {
-      const numTracksToRemove = numTracks - this._tracks.length;
-      const startIndex = this._tracks.length - numTracksToRemove - 1;
-      this._tracks.splice(startIndex, numTracksToRemove);
-    } else {
-      const numTracksToAdd = numTracks - this._numTracks;
-      Array(numTracksToAdd)
-        .fill(0)
-        .forEach(() => {
-          this._tracks.push(new TrackState(this._numSteps));
-        });
-    }
-  }
-
   private _scheduleNoteForStep(stepIndex: number, time: number): void {
     if (this._audioEngine == null) {
       return;
@@ -104,18 +95,30 @@ export default class SequencerEngine {
       if (!step.active || this._audioEngine == null) {
         return;
       }
-      if (index === 0) {
-        this._audioEngine.scheduleKick(time, 0.4);
-      } else if (index === 1) {
-        this._audioEngine.scheduleSnare(time);
-      } else if (index === 2) {
-        this._audioEngine.scheduleClosedHH(time);
-      } else {
-        this._audioEngine.scheduleNote(
-          track.oscType,
-          time,
-          semitoneToHz(step.coarsePitch)
-        );
+      switch (track.generatorType) {
+        case TrackType.Kick:
+          this._audioEngine.scheduleKick(time, 0.4);
+          break;
+        case TrackType.Snare:
+          this._audioEngine.scheduleSnare(time);
+          break;
+        case TrackType.ClosedHH:
+          this._audioEngine.scheduleClosedHH(time);
+          break;
+        case TrackType.SineBleep:
+          this._audioEngine.scheduleNote(
+            "sine",
+            time,
+            semitoneToHz(step.coarsePitch)
+          );
+          break;
+        case TrackType.SquareBleep:
+          this._audioEngine.scheduleNote(
+            "square",
+            time,
+            semitoneToHz(step.coarsePitch)
+          );
+          break;
       }
     });
   }
--- sequencer-client/src/state/SongState.tsx
+++ sequencer-client/src/state/SongState.tsx
@@ -4,14 +4,14 @@ import { StepState } from "../audio/SequencerEngine";
 import { sequencerEngine } from "./AudioEngineState";
 
 const NUM_STEPS = 16;
-const NUM_TRACKS = 5;
 
 export default class SongState {
   private readonly _steps: Array<Array<RecoilState<StepState>>>;
 
-  constructor(numTracks: number) {
-    sequencerEngine.setNumTracks(numTracks);
-    this._steps = new Array<Array<RecoilState<StepState>>>(numTracks);
+  constructor() {
+    this._steps = new Array<Array<RecoilState<StepState>>>(
+      sequencerEngine.numTracks
+    );
     for (const trackIndex of this._steps.keys()) {
       this._steps[trackIndex] = new Array<RecoilState<StepState>>(NUM_STEPS);
       for (const stepIndex of this._steps[trackIndex].keys()) {
@@ -33,4 +33,4 @@ export default class SongState {
   }
 }
 
-export const songState = new SongState(NUM_TRACKS);
+export const songState = new SongState();
