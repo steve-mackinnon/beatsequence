967a03f0590dbd06a834807e4bd9a0cc82699af7
Steve MacKinnon <smackinnon@izotope.com>
2022-10-05 07:55:10 -0400

    Clean up track controls styling

--- sequencer-client/src/components/TrackControls.tsx
+++ sequencer-client/src/components/TrackControls.tsx
@@ -1,8 +1,8 @@
 import React, { ReactElement } from "react";
-// import "../css/TrackControls.css";
 import { RecoilState, useRecoilState } from "recoil";
 import { TrackParams } from "../model/TrackParams";
-import { Button, Checkbox, FormControlLabel } from "@mui/material";
+import { Button } from "@mui/material";
+import { styled } from "@mui/system";
 
 interface TrackControlsProps {
   trackParams: RecoilState<TrackParams>;
@@ -11,34 +11,35 @@ interface TrackControlsProps {
   randomizePressed: () => void;
 }
 
+const TrackControlsContainer = styled("div")({
+  minWidth: 100,
+  maxWidth: 100,
+  display: "flex",
+  flexDirection: "row",
+  minHeight: 66,
+  maxHeight: 66,
+});
 export function TrackControls(props: TrackControlsProps): ReactElement {
   const [trackParams, setTrackParams] = useRecoilState<TrackParams>(
     props.trackParams
   );
 
-  const onMuteStateChanged = (event: any): void => {
+  const onMuteStateChanged = (_: any): void => {
     setTrackParams((current: TrackParams) => {
       const newTrackParams = { ...current };
-      newTrackParams.muted = event.target.checked;
+      newTrackParams.muted = !newTrackParams.muted;
       return newTrackParams;
     });
   };
 
   return (
-    <div>
-      <FormControlLabel
-        label="Mute"
-        control={
-          <Checkbox
-            checked={trackParams.muted}
-            onChange={onMuteStateChanged}
-            id="mute"
-          />
-        }
-      />
+    <TrackControlsContainer>
+      <Button onClick={onMuteStateChanged}>
+        {trackParams.muted ? "Unmute" : "Mute"}
+      </Button>
       <Button onClick={props.fourOnTheFloorPressed}>4x4</Button>
       <Button onClick={props.twoOnTheFloorPressed}>2x4</Button>
       <Button onClick={props.randomizePressed}>Rand</Button>
-    </div>
+    </TrackControlsContainer>
   );
 }
