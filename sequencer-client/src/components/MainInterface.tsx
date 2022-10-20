04fa220a13efac8cb88884cd2caf4374834639e4
Steve MacKinnon <smackinnon@izotope.com>
2022-10-20 07:04:37 -0400

    Toggle playback with space bar

--- sequencer-client/src/components/MainInterface.tsx
+++ sequencer-client/src/components/MainInterface.tsx
@@ -1,9 +1,21 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { TransportControls } from "./TransportControls";
 import "../css/MainInterface.css";
 import { TrackList } from "./TrackList";
+import { audioEngine } from "../engine";
 
 export default function MainInterface(): ReactElement {
+  const keydownListener = (event: KeyboardEvent): void => {
+    if (event.code.toLowerCase() === "space") {
+      audioEngine.playing = !audioEngine.playing;
+    }
+  };
+  useEffect(() => {
+    addEventListener("keydown", keydownListener);
+    return () => {
+      removeEventListener("keydown", keydownListener);
+    };
+  });
   return (
     <div className="MainInterface">
       <TransportControls />
