0ebdb10cf24a27ccd779f2ed2067319f17da2996
Steve MacKinnon <smackinnon@izotope.com>
2022-10-20 09:27:17 -0400

    Clean up layout of transport controls

--- sequencer-client/src/components/TransportControls.tsx
+++ sequencer-client/src/components/TransportControls.tsx
@@ -2,7 +2,8 @@ import React, { ReactElement } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { randomize } from "../features/steps/steps";
 import { togglePlayback, adjustTempo } from "../features/song/song";
-import { Button, Slider, InputLabel } from "@mui/material";
+import { Button, Slider, Input, Typography } from "@mui/material";
+import { Stack, Box } from "@mui/system";
 
 export function TransportControls(): ReactElement {
   const dispatch = useAppDispatch();
@@ -27,18 +28,42 @@ export function TransportControls(): ReactElement {
   };
 
   return (
-    <div>
-      <Button onClick={onPlayStopClick}>{playing ? "Stop" : "Play"}</Button>
-      <Button onClick={onRandomizeClick}>Rand</Button>
-      <Slider
-        name="Tempo"
-        id={`Tempo Slider`}
-        min={10}
-        max={999}
-        onChange={onTempoChange}
-        value={tempo}
-      />
-      <InputLabel htmlFor={`Tempo`}>{tempo}</InputLabel>
-    </div>
+    <Box
+      sx={{
+        width: "1183px",
+        flexDirection: "row",
+        display: "flex",
+      }}
+    >
+      <Stack direction="row" spacing={2} flex={1} alignItems="center">
+        <Typography id="tempo-slider">Tempo</Typography>
+        <Slider
+          name="Tempo"
+          id={`Tempo Slider`}
+          min={10}
+          max={200}
+          onChange={onTempoChange}
+          sx={{
+            width: 100,
+          }}
+          value={tempo}
+          size="small"
+          aria-labelledby="tempo-slider"
+        />
+        <Input
+          value={tempo}
+          onChange={onTempoChange}
+          size="small"
+          aria-labelledby="tempo-slider"
+          sx={{
+            width: 30,
+          }}
+        />
+      </Stack>
+      <Stack direction="row" spacing={2} alignItems="center" flex={2}>
+        <Button onClick={onPlayStopClick}>{playing ? "Stop" : "Play"}</Button>
+        <Button onClick={onRandomizeClick}>Rand</Button>
+      </Stack>
+    </Box>
   );
 }
