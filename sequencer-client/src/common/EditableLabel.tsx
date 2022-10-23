e6b3dc2df4b044982e865aceea3bcd4fd65ac43e
Steve MacKinnon <smackinnon@izotope.com>
2022-10-23 10:20:25 -0400

    Add EditableLabel to common components

---
+++ sequencer-client/src/common/EditableLabel.tsx
@@ -0,0 +1,47 @@
+import React, { ReactElement, useState } from "react";
+import { TextField } from "@mui/material";
+
+export interface EditableLabelProps {
+  onEditComplete: (value: string) => string;
+  getValue: () => string;
+}
+export function EditableLabel(props: EditableLabelProps): ReactElement {
+  const [value, setValue] = useState(props.getValue());
+  const [isEditing, setIsEditing] = useState(false);
+
+  const updateValue = (e: any): void => {
+    setIsEditing(false);
+    const name = e.target.value;
+    if (name == null || name === "") {
+      return;
+    }
+    const newValue = props.onEditComplete(e.target.value);
+    setValue(newValue);
+  };
+  const beginEditing = (_: any): void => {
+    setIsEditing(true);
+  };
+
+  const nameElement = isEditing ? (
+    <TextField
+      onBlur={updateValue}
+      onKeyDown={(e: any) => {
+        if (e.key === "Escape") {
+          setIsEditing(false);
+          return;
+        }
+        if (e.key === "Enter" || e.keyCode === 13) {
+          updateValue(e);
+        }
+      }}
+      onFocus={(e: any) => e.target.select()}
+      onAbort={(_: any) => setIsEditing(false)}
+      defaultValue={value}
+      autoFocus
+      size="small"
+    />
+  ) : (
+    <span onDoubleClick={beginEditing}>{value}</span>
+  );
+  return nameElement;
+}
--- sequencer-client/src/features/tracks/TrackInfoView.tsx
+++ sequencer-client/src/features/tracks/TrackInfoView.tsx
@@ -1,7 +1,8 @@
-import React, { ReactElement, useRef, useState } from "react";
+import React, { ReactElement } from "react";
 import { useAppSelector, useAppDispatch } from "../../hooks";
-import { Grid, TextField } from "@mui/material";
+import { Grid } from "@mui/material";
 import { setDisplayName } from "./tracks";
+import { EditableLabel } from "../../common/EditableLabel";
 
 export interface TrackInfoProps {
   trackId: number;
@@ -11,50 +12,24 @@ export function TrackInfoView(props: TrackInfoProps): ReactElement {
   const trackName = useAppSelector(
     (state) => state.persistedReducer.tracks[props.trackId].displayName
   );
-  const [isEditingName, setIsEditingName] = useState(false);
-  const trackNameEditRef = useRef<HTMLInputElement>(null);
 
-  const updateTrackName = (e: any): void => {
-    setIsEditingName(false);
-    const name = e.target.value;
-    if (name == null || name === "") {
-      return;
-    }
+  const updateTrackName = (name: string): string => {
     dispatch(
       setDisplayName({
         trackId: props.trackId,
         name,
       })
     );
+    return name;
   };
-  const beginEditingTrackName = (_: any): void => {
-    setIsEditingName(true);
-  };
-
-  const nameElement = isEditingName ? (
-    <TextField
-      onBlur={updateTrackName}
-      onKeyDown={(e: any) => {
-        if (e.key === "Escape") {
-          setIsEditingName(false);
-          return;
-        }
-        if (e.key === "Enter" || e.keyCode === 13) {
-          updateTrackName(e);
-        }
-      }}
-      onFocus={(e: any) => e.target.select()}
-      onAbort={(_: any) => setIsEditingName(false)}
-      defaultValue={trackName}
-      ref={trackNameEditRef}
-      autoFocus
-    />
-  ) : (
-    <p onDoubleClick={beginEditingTrackName}>{trackName}</p>
-  );
   return (
     <Grid container minWidth={100} maxWidth={100}>
-      <Grid xs={8}>{nameElement}</Grid>
+      <Grid xs={8}>
+        <EditableLabel
+          onEditComplete={updateTrackName}
+          getValue={() => trackName}
+        />
+      </Grid>
     </Grid>
   );
 }
