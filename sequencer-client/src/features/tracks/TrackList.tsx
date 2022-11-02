aab9436c65e5b17ebcd7fa9c143047b107d4585a
Steve MacKinnon <smackinnon@izotope.com>
2022-11-02 16:12:32 -0400

    Attempt to fix touch handling of sliders

--- sequencer-client/src/features/tracks/TrackList.tsx
+++ sequencer-client/src/features/tracks/TrackList.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useRef, useEffect } from "react";
+import React, { ReactElement } from "react";
 import { TrackContainer } from "./TrackContainer";
 import { useAppSelector } from "../../hooks";
 import "../../css/SequencerTrack.css";
@@ -6,16 +6,7 @@ import { Box } from "@mui/system";
 
 export function TrackList(): ReactElement {
   const numTracks = useAppSelector((state) => state.tracks.length);
-  const containerRef = useRef<HTMLElement>(null);
 
-  useEffect(() => {
-    // Automatically focus the container so that the initial touch event
-    // can be used to scroll.
-    if (containerRef.current == null) {
-      return;
-    }
-    containerRef.current.focus();
-  });
   const trackIndices = Array.from(Array(numTracks).keys());
   const tracks = trackIndices.map((index: number) => (
     <TrackContainer key={`track${index}`} trackIndex={index} />
@@ -27,7 +18,6 @@ export function TrackList(): ReactElement {
         height: "78%",
         overflowY: "scroll",
       }}
-      ref={containerRef}
     >
       {tracks}
     </Box>
