0443719dea533d82b1be0d7c6d30a069d929d287
Steve MacKinnon <smackinnon@izotope.com>
2022-11-12 21:06:39 -0500

    Use Formik and Yup for form validation in the create account page

--- sequencer-client/pages/account/create/index.tsx
+++ sequencer-client/pages/account/create/index.tsx
@@ -1,75 +1,108 @@
-import { TextField, Button, Typography } from "@mui/material";
-import { ReactElement, useState } from "react";
+import { TextField, Button, Typography, FormGroup } from "@mui/material";
+import { ReactElement } from "react";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "../../../src/firebase";
-import { Stack } from "@mui/system";
+import { Stack, styled } from "@mui/system";
+import { Formik } from "formik";
 import Link from "next/link";
+import * as Yup from "yup";
+
+const Spacer = styled("div")(
+  ({ theme }) => `
+  height: 1.5rem
+  `
+);
 
 export default function CreateAccount(): ReactElement {
   const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(auth);
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const isSubmitButtonEnabled = (): boolean => {
-    return email.length > 0 && password.length > 0 && user == null && !loading;
-  };
-  const onSubmitClick = (_e: any): void => {
-    createUserWithEmailAndPassword(email, password).catch((error) => {
-      console.log(error);
-    });
-  };
   return (
-    <Stack spacing={3} alignItems="center">
-      <Typography variant="overline">Beatsequence</Typography>
-      <Typography variant="subtitle1">Create your free account</Typography>
-      <TextField
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setEmail(e.target.value);
-        }}
-        variant="outlined"
-        value={email}
-        name="Email"
-        type="text"
-        label="Email"
-        sx={{
-          minWidth: "320px",
-        }}
-      />
-      <TextField
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setPassword(e.target.value);
-        }}
-        variant="outlined"
-        value={password}
-        name="Password"
-        type="password"
-        label="Password"
-        sx={{
-          minWidth: "320px",
-        }}
-      />
-      <Button
-        variant="contained"
-        disabled={!isSubmitButtonEnabled()}
-        onClick={onSubmitClick}
-        sx={{
-          minWidth: "320px",
-        }}
-      >
-        Create Account
-      </Button>
-      <Typography>
-        Already have an account? <Link href="/account/login">Login here</Link>
-      </Typography>
-      {loading && <Typography>Creating account...</Typography>}
-      {user != null && !user.user.emailVerified && (
-        <Typography>Account created successfully.</Typography>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={Yup.object({
+        email: Yup.string()
+          .email("Invalid email address")
+          .required("Email required"),
+        password: Yup.string()
+          .required("Password required")
+          .min(6, "Password must be at least 6 characters long."),
+      })}
+      onSubmit={(values) => {
+        void createUserWithEmailAndPassword(values.email, values.password);
+      }}
+    >
+      {(formik) => (
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={3} alignItems="center">
+            <Typography variant="overline">Beatsequence</Typography>
+            <Typography variant="subtitle1">
+              Create your free account
+            </Typography>
+            <Stack spacing={1} alignItems="left">
+              <TextField
+                variant="outlined"
+                id="email"
+                type="email"
+                label="Email"
+                sx={{
+                  minWidth: "320px",
+                }}
+                {...formik.getFieldProps("email")}
+              />
+              {(formik.touched.email ?? false) &&
+              formik.errors.email != null ? (
+                <Typography variant="subtitle2" color="red">
+                  {formik.errors.email}
+                </Typography>
+              ) : (
+                <Spacer />
+              )}
+              <TextField
+                variant="outlined"
+                type="password"
+                label="Password"
+                sx={{
+                  minWidth: "320px",
+                }}
+                {...formik.getFieldProps("password")}
+              />
+              {(formik.touched.password ?? false) &&
+              formik.errors.password != null ? (
+                <Typography variant="subtitle2" color="red">
+                  {formik.errors.password}
+                </Typography>
+              ) : (
+                <Spacer />
+              )}
+            </Stack>
+            <Button
+              variant="contained"
+              type="submit"
+              disabled={!formik.isValid}
+              sx={{
+                minWidth: "320px",
+              }}
+            >
+              Create Account
+            </Button>
+            <Typography>
+              Already have an account?{" "}
+              <Link href="/account/login">Login here</Link>
+            </Typography>
+            {loading && <Typography>Creating account...</Typography>}
+            {user != null && !user.user.emailVerified && (
+              <Typography>Account created successfully.</Typography>
+            )}
+            {error != null && (
+              <Typography>Account creation failed: {error.message}</Typography>
+            )}
+          </Stack>
+        </form>
       )}
-      {error != null && (
-        <Typography>Account creation failed: {error.message}</Typography>
-      )}
-    </Stack>
+    </Formik>
   );
 }
