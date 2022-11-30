a242118d30d840441b4dc0cf23842860d19a6488
Steve MacKinnon <smackinnon@izotope.com>
2022-11-29 21:45:57 -0500

    Remove duplicated heads and unused files

--- sequencer-client/src/pages/Home.tsx
+++ sequencer-client/src/pages/Home.tsx
@@ -40,23 +40,6 @@ export default function Home(): ReactElement {
   };
   return (
     <div>
-      <head key="landingpageHead">
-        <title>
-          Beatsequence - experimental beat making tool for the browser
-        </title>
-        <meta
-          name="description"
-          content="Make beats in your browser with this powerful step sequencer and synthesis engine. Works seamlessly on mobile or desktop."
-        />
-        <meta
-          name="og:title"
-          property="og:title"
-          content="Beatsequence - experimental beat making tool for the browser"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="canonical" href="https://beatsequence.com/" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
       <MainContainer>
         <Typography variant="h1">
           The{" "}
