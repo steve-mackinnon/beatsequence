8cf207bc606e7a70b0be7ca75fd9ac71604c7457
Steve MacKinnon <smackinnon@izotope.com>
2022-12-28 22:58:01 -0500

    Remove unreachable loading code from audio workstation

--- sequencer-client/src/components/AudioWorkstation.tsx
+++ sequencer-client/src/components/AudioWorkstation.tsx
@@ -1,19 +1,14 @@
-import React, { ReactElement, useContext, useEffect } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { FooterControls } from "./FooterControls";
 import styles from "../styles/AudioWorkstation.module.css";
 import { TrackList } from "../features/tracks/TrackList";
 import { useAppDispatch } from "../hooks";
 import { togglePlayback } from "../features/song/song";
 import { HeaderControls } from "./HeaderControls";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { AuthContext } from "../context/authContext";
-import { Typography } from "@mui/material";
 import { hotkeySuppressor } from "../hotkeySuppressor";
 
 export default function AudioWorkstation(): ReactElement {
   const dispatch = useAppDispatch();
-  const auth = useContext(AuthContext);
-  const [, loading] = useAuthState(auth);
 
   useEffect(() => {
     return () => {
@@ -36,9 +31,6 @@ export default function AudioWorkstation(): ReactElement {
       removeEventListener("keydown", keydownListener, true);
     };
   });
-  if (loading) {
-    return <Typography>Loading...</Typography>;
-  }
   return (
     <div className={styles.AudioWorkstation}>
       <HeaderControls />
