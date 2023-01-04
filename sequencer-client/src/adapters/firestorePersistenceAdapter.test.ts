aaf01d136b97ff30109324ace7297838051d0b3f
Steve MacKinnon <smackinnon@izotope.com>
2023-01-04 13:24:14 -0500

    Fix session upgrade from "v2" format

--- sequencer-client/src/adapters/firestorePersistenceAdapter.test.ts
+++ sequencer-client/src/adapters/firestorePersistenceAdapter.test.ts
@@ -66,7 +66,7 @@ test("serialize and deserialize", () => {
   expect(extractedProject).toEqual(project);
 });
 
-interface OldFormatStep {
+interface StepV1 {
   enabled: boolean;
   params: {
     coarsePitch: number;
@@ -75,8 +75,8 @@ interface OldFormatStep {
   trackId: number;
 }
 
-test("deserialize old format", () => {
-  const steps = new Array<OldFormatStep>();
+test("deserialize v1 format", () => {
+  const steps = new Array<StepV1>();
   // Populate steps with 2 tracks, each with 16 steps
   for (let trackIndex = 0; trackIndex < 2; ++trackIndex) {
     for (let stepIndex = 0; stepIndex < 16; ++stepIndex) {
@@ -122,4 +122,62 @@ test("deserialize old format", () => {
   );
   // New step format has coarsePitch
   expect(deserialized.pattern.steps[0][0].note).toEqual("C4");
+});
+
+interface StepV2 {
+  enabled: boolean;
+  coarsePitch: number;
+}
+
+test("deserialize v2 format", () => {
+  const steps = new Array<StepV2>();
+  // Populate steps with 2 tracks, each with 16 steps
+  for (let trackIndex = 0; trackIndex < 2; ++trackIndex) {
+    for (let stepIndex = 0; stepIndex < 16; ++stepIndex) {
+      steps.push({
+        enabled: true,
+        coarsePitch: 13,
+      });
+    }
+  }
+  const track1: Track = {
+    muted: false,
+    generatorType: GeneratorType.Kick,
+    displayName: "Kick",
+    generatorParams: {
+      triggerProbability: 100,
+      gain: 0.5,
+      decayTime: 0.3,
+    },
+    paramViewVisible: true,
+    soloed: false,
+  };
+  const track2: Track = {
+    muted: true,
+    generatorType: GeneratorType.SineBleep,
+    displayName: "Sine",
+    generatorParams: {
+      triggerProbability: 60,
+      gain: 0.8,
+      decayTime: 0.5,
+    },
+    paramViewVisible: true,
+    soloed: true,
+  };
+  const song: any = {
+    name: "My song",
+    params: {
+      tempo: 42,
+    },
+    playing: false,
+  };
+  const project: any = {
+    song,
+    steps,
+    tracks: [track1, track2],
+  };
+  const deserialized = extractProjectFromPayload(project);
+
+  // New step format has coarsePitch
+  expect(deserialized.pattern.steps[0][0].note).toEqual("C#4");
 });
